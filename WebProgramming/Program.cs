358d110130da469f0e0f40e17eb24d3778b98572
alpersalih <alpersalihaktas@gmail.com>
2024-11-29 16:00:18 +0300

    Edit-1

--- WebProgramming/Program.cs
+++ WebProgramming/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Services}/{action=Services}/{id?}");
 
 app.Run();
