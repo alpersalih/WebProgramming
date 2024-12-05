d7e808c2a615716fc49f8047b39d98560c40022b
alpersalih <alpersalihaktas@gmail.com>
2024-12-05 23:44:45 +0300

    Edit-3

---
+++ WebProgramming/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace WebProgramming.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+        public string Specialization { get; set; } 
+        public TimeSpan StartTime { get; set; } 
+        public TimeSpan EndTime { get; set; } 
+    }
+
+}
