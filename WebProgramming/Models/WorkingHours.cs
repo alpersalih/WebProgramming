1e8ff1e860bfa2c6f69308f2d24fd52dc4d33f81
alpersalih <alpersalihaktas@gmail.com>
2024-12-18 23:54:28 +0300

    Edit-6

--- WebProgramming/Models/WorkingHours.cs
+++ WebProgramming/Models/WorkingHours.cs
@@ -4,6 +4,7 @@
     {
         public int WorkingHoursID { get; set; }
         public string Day { get; set; }
+        public string Days { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
     }
